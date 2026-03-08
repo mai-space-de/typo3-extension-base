860bae4d620f0168c6fd44ac2837951c55cd1bf1
maispace <joel@maispace.de>
2026-03-08 17:17:05 +0100

    [FEATURE] Update TYPO3 configuration files and clean up unused imports

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,15 +11,3 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginFootnote'] = '© 2023
 ExtensionManagementUtility::addUserTSConfig(
     'EXT:base/Configuration/TsConfig/user.tsconfig'
 );
-
-// Load base typoscript
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'base',
-    'setup',
-    "@import 'EXT:base/Configuration/TypoScript/constants.typoscript'"
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
-    'base',
-    'setup',
-    "@import 'EXT:base/Configuration/TypoScript/setup.typoscript'"
-);
