77904c2b323dedd6ad9aebd4077f6764c56d744c
maispace <joel@maispace.de>
2026-05-15 23:07:15 +0200

    feat: add shared site set scaffold and generic icons; refresh icon registry

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -7,4 +7,20 @@ return [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:mai_base/Resources/Public/Icons/Extension.svg',
     ],
+    'mai-extension' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mai_base/Resources/Public/Icons/extension.svg',
+    ],
+    'mai-content' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mai_base/Resources/Public/Icons/generic_content.svg',
+    ],
+    'mai-table' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mai_base/Resources/Public/Icons/generic_table.svg',
+    ],
+    'mai-backend-module' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:mai_base/Resources/Public/Icons/generic_backend_module.svg',
+    ],
 ];
